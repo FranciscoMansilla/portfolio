4b511d1871fe5ab13a981cbfcff6f0d4d5b6cff0
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-09-26 21:07:47 -0300

    change cv url

--- src/components/commons/Contact.tsx
+++ src/components/commons/Contact.tsx
@@ -41,7 +41,7 @@ const Contact = ()=>{
       <a 
         className=" shadow-md w-8/12 md:w-4/12 mt-4 bg-gray-400 hover:bg-gray-300 text-white font-F-Medium py-2 px-4 border-b-4 border-gray-600 hover:border-gray-400 rounded hover:text-black
         hover:scale-105 ease-in-out duration-200"
-        href="https://github.com/FranciscoMansilla"
+        href="https://drive.google.com/file/d/1km7lIca4hvhakbT-Bk9Y7BcUMO-sOPHR/view?usp=sharing"
         target="_blank">
         Download CV 
       </a>
